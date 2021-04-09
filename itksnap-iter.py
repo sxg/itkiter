440ce25c9abd0f8835a295026e2e5502269fef47
Satyam Ghodasara <sghodas@gmail.com>
2021-04-08 22:34:53 -0400

    Add CLI options

--- itksnap-iter.py
+++ itksnap-iter.py
@@ -7,7 +7,10 @@ import pathlib
 
 @click.command()
 @click.option('-p', '--path', default=pathlib.Path().absolute(), help='Path to the folders containing images and segmentations.')
-def cli(path):
+@click.option('-i', '--image-suffix', required=True, help='Suffix to add to the folder name to create the main image file name.')
+@click.option('-s', '--segmentation-suffix', required=True, help='Suffix to add to the folder name to create the segmentation file name.')
+@click.option('-a', '--additional-image-suffix', required=True, multiple=True, help='Suffix to add to the folder name to create the file name for an additional image.')
+def cli(path, image_suffix, segmentation_suffix, additional_image_suffix):
     print(path)
 
 if __name__ == '__main__':
