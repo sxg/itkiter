a60669fe7153a9a2eea7389fcfd158876fa95e53
Satyam Ghodasara <sghodas@gmail.com>
2021-04-08 22:55:36 -0400

    Update CLI options

--- itksnap-iter.py
+++ itksnap-iter.py
@@ -6,12 +6,13 @@ import click
 import pathlib
 
 @click.command()
-@click.option('-p', '--path', default=pathlib.Path().absolute(), help='Path to the folders containing images and segmentations.')
+@click.option('-p', '--path', default=pathlib.Path().absolute(), help='Path to the folders containing images and segmentations. Defaults to the current path.')
 @click.option('-i', '--image-suffix', required=True, help='Suffix to add to the folder name to create the main image file name.')
 @click.option('-s', '--segmentation-suffix', required=True, help='Suffix to add to the folder name to create the segmentation file name.')
-@click.option('-a', '--additional-image-suffix', required=True, multiple=True, help='Suffix to add to the folder name to create the file name for an additional image.')
 def cli(path, image_suffix, segmentation_suffix, additional_image_suffix):
     print(path)
+@click.option('-a', '--additional-image-suffix', required=True, multiple=True, help='Suffix(es) to add to the folder name to create the file name for an additional image.')
+@click.option('-e', '--extension', default='.nii.gz', help='Extension for image and segmentation files. Defaults to .nii.gz.')
 
 if __name__ == '__main__':
     cli()
